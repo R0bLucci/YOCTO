a1175050749dbb1bcab99bb46ddd18533d40cd9a
Robert Ntim <robyntimjnr@yahoo.it>
2016-06-11 04:13:46 +0100

    Go page to display trains routes

---
+++ html/go.php
@@ -0,0 +1,20 @@
+<?php include_once "../template/header.php"; ?>
+
+<?php include_once "../model/transportapi.php" ?>
+<div>
+
+	<div id="map">
+
+
+
+	</div>
+
+	<div id="showTT">
+	</div>
+</div>
+
+<a href="index.php"> Station Analyser </a>
+
+<script src="js/journey.js" ></script>
+
+<?php include_once "../template/footer.php"; ?>
